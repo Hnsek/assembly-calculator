0a0020ef57c05c2ce82ef9021195ee072c114404
Hnsek <83185963+Hnsek@users.noreply.github.com>
2021-04-27 21:07:49 -0300

    Update assembly-calculator.asm

--- assembly-calculator.asm
+++ assembly-calculator.asm
@@ -2,8 +2,11 @@
 
 x: .asciiz "Digite o primeiro valor: " # armazena a string do primeiro valor
 y: .asciiz "Digite o segundo valor: " # armazena a string do segundo valor
-resultado: .asciiz "Resultado: "  # armazena a string do resultado da operação
-menu: .asciiz "Selecione uma opção (1-soma, 2-subtração, 3-multiplicação, 4-divisão): " # armazena a string de opções
+resultado: .asciiz "Resultado: "  # armazena a string do resultado da operaÃ§Ã£o
+menu: .asciiz "Selecione uma opÃ§Ã£o (1-soma, 2-subtraÃ§Ã£o, 3-multiplicaÃ§Ã£o, 4-divisÃ£o, 5-raiz): " # armazena a string de opÃ§Ãµes
+mraiz: .asciiz "Insira o nÃºmero a ser calculada a raiz quadrada:"
+resultadoa: .asciiz "Resultado estÃ¡ entre "
+e: .asciiz " e "
 
 ##################################
 .text
@@ -11,116 +14,183 @@ menu: .asciiz "Selecione uma opção (1-soma, 2-subtração, 3-multiplicação, 4-divi
 .macro soma # macro da soma
 la $a0,x # armazena x em $a0 
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-li $v0,5 # lê um inteiro
-syscall
+syscall # executa a chamada de sistema
+li $v0,5 # lÃª um inteiro
+syscall # executa a chamada de sistema
 move $t0,$v0 # move o inteiro inserido para $t0
 
 la $a0,y # armazena y em $a0 
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-li $v0,5 # lê um inteiro
-syscall
+syscall # executa a chamada de sistema
+li $v0,5 # lÃª um inteiro
+syscall # executa a chamada de sistema
 move $t1,$v0 # move o inteiro inserido para $t0
 
 add $t2,$t0,$t1 # realiza a soma entre $t0 e $t1 e armazena em $t2
 la $a0,resultado # armazena resultado em $a0
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
+syscall # executa a chamada de sistema
 move $a0,$t2 # carrega o resultado da soma ($t2) em $a0
 li $v0,1 # exibe o resultado
-syscall
+syscall # executa a chamada de sistema
 li $v0,10 # chamada de sistema para encerrar o programa
-syscall
+syscall # executa a chamada de sistema
 
 .end_macro # encerramento da macro da soma
 
 ##################################
 
-.macro subtracao # macro da subtração
+.macro subtracao # macro da subtraÃ§Ã£o
 la $a0,x # armazena x em $a0 
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-li $v0,5 # lê um inteiro
-syscall
+syscall # executa a chamada de sistema
+li $v0,5 # lÃª um inteiro
+syscall # executa a chamada de sistema
 move $t0,$v0 # move o inteiro inserido para $t0
 
 la $a0,y # armazena y em $a0 
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-li $v0,5 # lê um inteiro
-syscall
+syscall # executa a chamada de sistema
+li $v0,5 # lÃª um inteiro
+syscall # executa a chamada de sistema
 move $t1,$v0 # move o inteiro inserido para $t0
 
-sub $t2,$t0,$t1 # realiza a subtração entre $t0 e $t1 e armazena em $t2
+sub $t2,$t0,$t1 # realiza a subtraÃ§Ã£o entre $t0 e $t1 e armazena em $t2
 la $a0,resultado # armazena resultado em $a0
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-move $a0,$t2 # carrega o resultado da subtração ($t2) em $a0
+syscall # executa a chamada de sistema
+move $a0,$t2 # carrega o resultado da subtraÃ§Ã£o ($t2) em $a0
 li $v0,1 # exibe o resultado
-syscall
+syscall # executa a chamada de sistema
 li $v0,10 # chamada de sistema para encerrar o programa
-syscall
+syscall # executa a chamada de sistema
 
-.end_macro # encerramento da macro da subtração
+.end_macro # encerramento da macro da subtraÃ§Ã£o
 
 ##################################
 
-.macro multiplicacao # macro da multiplicação
+.macro multiplicacao # macro da multiplicaÃ§Ã£o
 la $a0,x # armazena x em $a0 
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-li $v0,5 # lê um inteiro
-syscall
+syscall # executa a chamada de sistema
+li $v0,5 # lÃª um inteiro
+syscall # executa a chamada de sistema
 move $t0,$v0 # move o inteiro inserido para $t0
 
 la $a0,y # armazena y em $a0 
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
 syscall
-li $v0,5 # lê um inteiro
-syscall
+li $v0,5 # lÃª um inteiro
+syscall # executa a chamada de sistema
 move $t1,$v0 # move o inteiro inserido para $t0
 
-mul $t2,$t0,$t1 # realiza a multiplicação entre $t0 e $t1 e armazena em $t2
+mul $t2,$t0,$t1 # realiza a multiplicaÃ§Ã£o entre $t0 e $t1 e armazena em $t2
 la $a0,resultado # armazena resultado em $a0
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-move $a0,$t2 # carrega o resultado da multiplicação ($t2) em $a0
+syscall # executa a chamada de sistema
+move $a0,$t2 # carrega o resultado da multiplicaÃ§Ã£o ($t2) em $a0
 li $v0,1 # exibe o resultado
 syscall
 li $v0,10 # chamada de sistema para encerrar o programa
-syscall
+syscall # executa a chamada de sistema
 
-.end_macro # encerramento da macro da multiplicação
+.end_macro # encerramento da macro da multiplicaÃ§Ã£o
 
 ##################################
 
-.macro divisao # macro da divisão
+.macro divisao # macro da divisÃ£o
 la $a0,x # armazena x em $a0 
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-li $v0,5 # lê um inteiro
-syscall
+syscall # executa a chamada de sistema
+li $v0,5 # lÃª um inteiro
+syscall # executa a chamada de sistema
 move $t0,$v0 # move o inteiro inserido para $t0
 
 la $a0,y # armazena y em $a0 
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-li $v0,5 # lê um inteiro
-syscall
+syscall # executa a chamada de sistema
+li $v0,5 # lÃª um inteiro
+syscall # executa a chamada de sistema
 move $t1,$v0 # move o inteiro inserido para $t0
 
-div $t2,$t0,$t1 # realiza a divisão entre $t0 e $t1 e armazena em $t2
+div $t2,$t0,$t1 # realiza a divisÃ£o entre $t0 e $t1 e armazena em $t2
 la $a0,resultado # armazena resultado em $a0
 li $v0,4 # chamada do sistema para mostrar a string contida em $a0
-syscall
-move $a0,$t2 # carrega o resultado da divisão ($t2) em $a0
+syscall # executa a chamada de sistema
+move $a0,$t2 # carrega o resultado da divisÃ£o ($t2) em $a0
 li $v0,1 # exibe o resultado
-syscall
+syscall # executa a chamada de sistema
 li $v0,10 # chamada de sistema para encerrar o programa
-syscall
+syscall # executa a chamada de sistema
 
-.end_macro # encerramento da macro da divisão
+.end_macro # encerramento da macro da divisÃ£o
+
+##################################
+
+.macro raiz #macro raiz
+la $a0,mraiz #armazena mraiz em $a0
+li $v0,4 #chamda de sistema para imprimir a string contida em $a0
+syscall # executa a chamada de sistema
+
+li $v0,5 #chamada de sistema para ler um inteiro
+syscall # executa a chamada de sistema
+move $t0,$v0 #mover o valor inserido em $v0 para $t0
+
+li $t1,1 #define $t1 como 1
+li $t2,1 #define $t2 como 1
+
+
+main: #label main
+mul $t3,$t2,$t1 #multiplica $t2 com $t2 e armazena em $t3 
+
+bgt $t3,$t0,maior #se $t3 Ã© maior do que $t0,pule para o label maior.
+bne $t3,$t0,nao #se %t3 nÃ£o Ã© igual a $t0,pule para o label nao
+
+
+
+r: #label r
+la $a0,resultado #armazenar resultado em $a0
+li $v0,4 #chamda de sistema para imprimir a string contida em $a0
+syscall # executa a chamada de sistema
+move $a0,$t2 #mover o valor contido em $t2 para $a0
+li $v0,1 #chamda de sistema para imprimir um inteiro
+syscall # executa a chamada de sistema
+li $v0,10 #chamda de sistema para encerar o promgrama 
+syscall # executa a chamada de sistema
+
+
+
+maior: #label maior
+la $a0,resultadoa #armazena resultadoa em $a0
+li $v0,4 #chamada de sistema para imprimir a string contida de $a0
+syscall # executa a chamada de sistema
+
+sub $t3,$t2,1 #subtrai $t2 por 1 e armazena em $t3
+move $a0,$t3 #move o valor de $te para $a0
+li $v0,1 #chamada de sistema para imprimir o valor contido em $a0
+syscall # executa a chamada de sistema
+
+la $a0,e #armazena em $a0 a string contidade em 'e'
+li $v0,4 #chamada de sistema para imprimir a string contidade em $a0
+syscall # executa a chamada de sistema
+
+move $a0,$t2 #mover o valor contido em $t2 para $a0
+li $v0,1 #chamada de sistema para imprimir o valor contido em $a0
+syscall # executa a chamada de sistema
+
+
+
+
+
+li $v0,10 #chamada de sistema para encerrar o programa
+syscall # executa a chamada de sistema
+
+nao: #label nao
+
+add $t1,$t1,1 #incrementar 1 em $t1
+add $t2,$t2,1 #incrementar 1 em $t2
+j main #voltar para o label main
+.end_macro
 
 ##################################
 
@@ -130,14 +200,18 @@ main:
 la $a0,menu # armazena em $a0 a string contida em menu
 li $v0,4 # chamada de sistema para mostrar a string contida em $a0
 syscall # executa a chamada de sistema
-li $v0,5 # lê um inteiro
+li $v0,5 # lÃª um inteiro
 syscall # executa a chamada de sistema
 move $t0,$v0 # move o inteiro inserido em $v0 para $t0
 
-beq $t0,1,soma # se $t0 for igual a 1, pula para a label soma, se não, segue o programa
+beq $t0,1,soma # se $t0 for igual a 1, pula para a label soma, se nÃ£o, segue o programa
 beq $t0,2,subtracao # se $t0 for igual a 2, pula para a label subtracao
 beq $t0,3,multiplicacao # se $t0 for igual a 3, pula para a label multiplicacao
 beq $t0,4,divisao # se $t0 for igual a 4, pula para a label divisao
+beq $t0,5,raiz # se $t0 for igual a 4, pula para a label raiz
+
+li $v0,10 # chamada de sistema que encerra o programa
+syscall # executa a chamada de sistema
 
 soma:
 soma # chama a macro soma
@@ -156,5 +230,10 @@ syscall # executa a chamada de sistema
 
 divisao:
 divisao # chama a macro divisao
+li $v0,10 # chamada de sistema que encerra o programa
+syscall # executa a chamada de sistema
+
+raiz:
+raiz # chama a macro divisao
 li $v0,10 # chamada de sistema que encerra o programa
 syscall # executa a chamada de sistema
